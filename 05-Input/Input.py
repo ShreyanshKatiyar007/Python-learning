5410cef1300edf779b2a37262a4e93a30a7f3c1d
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-11 10:22:59 +0530

    Create Input.py

---
+++ 05-Input/Input.py
@@ -0,0 +1,9 @@
+name = input("enter your name:")
+
+# Result for input() is always a string.
+val = input("enter any numeric value:")
+print(type(val), val)  
+
+# int value
+var = int(input("enter any numeric value:"))
+print(type(var), var)  
