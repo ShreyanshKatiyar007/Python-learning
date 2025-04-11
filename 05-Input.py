dad0daa8d01766f51a1b05359297b44d24deead0
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-11 10:22:22 +0530

    Delete 05-Input.py

--- 05-Input.py
+++
@@ -1,9 +0,0 @@
-name = input("enter your name:")
-
-# Result for input() is always a string.
-val = input("enter any numeric value:")
-print(type(val), val)  
-
-# int value
-var = int(input("enter any numeric value:"))
-print(type(var), var)  
