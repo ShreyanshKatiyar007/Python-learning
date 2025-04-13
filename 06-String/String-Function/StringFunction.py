7f1fc1a99f973b0213eed051f5725f2a1d1bb278
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-13 15:26:16 +0530

    Create StringFunction.py

---
+++ 06-String/String-Function/StringFunction.py
@@ -0,0 +1,12 @@
+# String Functions
+str = "i am studying python"
+
+print(str.endswith("on"))  # returns true if string ends with substring.
+
+print(str.capitalize())  # capitalizes first character.
+
+print(str.replace("python","javascript"))  # replaces all occurrences of old with new.
+
+print(str.find("s"))  # returns first index of first occurrence.
+
+print(str.count("i"))  # count the occurrence of substring.
