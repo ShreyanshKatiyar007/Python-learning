ef6d461396634fdbea075a8ae9b7dc97980b0022
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-13 15:25:37 +0530

    Create Slicing.py

---
+++ 06-String/Slicing/Slicing.py
@@ -0,0 +1,12 @@
+str = "shreyansh"
+# accessing parts of a string
+
+# print(str[:3])
+print(str[0:3]) #ending index is not included
+
+print(str[5:9])
+
+# print(str[0:])
+print(str[0:len(str)])
+
+print(str[-3:-1]) # negative indexing
