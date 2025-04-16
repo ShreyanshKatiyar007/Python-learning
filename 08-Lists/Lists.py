6abef9fc7d24513beb830727cdadd5a83b2973b1
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-16 23:52:24 +0530

    Create Lists.py

---
+++ 08-Lists/Lists.py
@@ -0,0 +1,17 @@
+marks = [94.5, 87.5, 99.1, 70.5, 87.8]
+
+print(marks)          
+
+print(type(marks ))
+
+print(marks[0])      # index value
+
+print(len(marks))    # returns length
+
+# List with different data types
+student = ["Alex", 98.25, 19, "Delhi"]
+print(student)
+
+#  list are mutable
+student[0] = "John"  # changes the value 
+print(student)
