4d26ea4897722063ebd373e76bfe697c8e6da1ee
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-16 23:53:10 +0530

    Create Method.py

---
+++ 08-Lists/Method/Method.py
@@ -0,0 +1,22 @@
+list = [10,25,17]
+
+list.append(20)     # adds an element at the end
+print(list)
+
+list.sort()         # sorts in ascending order
+print(list)
+
+list.sort(reverse = True)   # sorts in descending order
+print(list)
+
+list.reverse()      # reverse list
+print(list)
+
+list.insert(1,30)   # insert element at given index
+print(list)
+
+list.remove(25)     # remove first occurence of element
+print(list)
+
+list.pop(2)         # remove element at given index
+print(list)
