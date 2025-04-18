f685b478ffec042542681d115e11780aa06ea3cd
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-18 11:27:08 +0530

    Create Tuples.js

---
+++ 09-Tuples/Tuples.js
@@ -0,0 +1,14 @@
+tuple = (10,20,30,40)
+# tuples are immutable
+
+print(type(tuple))
+
+print(tuple)
+
+print(tuple[2])  # return index value
+
+tup = ()
+print(tup)       # empty tuple
+
+tupl = (1,)      # tuple with single value
+print(tupl)
