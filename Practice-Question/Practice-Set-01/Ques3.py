f65f4a4494636f34124b69ea2e40efb9c5548c24
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-23 23:36:52 +0530

    Create Ques3.py

---
+++ Practice-Question/Practice-Set-01/Ques3.py
@@ -0,0 +1,5 @@
+# Write a Program to input 2 floating point numbers and print their average.
+
+a = float(input("Enter first floating point number:"))
+b = float(input("Enter second floating point number:"))
+print("Average is:", (a + b)/2)
