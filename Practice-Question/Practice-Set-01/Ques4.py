523464ce9cc5dcdd15bcba16556d84f85207fb4f
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-23 23:37:14 +0530

    Create Ques4.py

---
+++ Practice-Question/Practice-Set-01/Ques4.py
@@ -0,0 +1,6 @@
+''' Write a Program to input 2 numbers, a and b.
+    Print True if 'a' is greater than or equal to 'b'.If not print False. '''
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+print(a>=b)
