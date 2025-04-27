5afbf86e094c5b98ae5f2cd9dd1032c28a8fafd3
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-27 23:26:09 +0530

    Create Ques1.py

---
+++ Practice-Question/Practice-Set-03/Ques1.py
@@ -0,0 +1,14 @@
+# Write a Program to ask the user names of their 3 favorite movies and store them in a list.
+
+print("Enter the name of your 3 favorite movies")
+movies = []
+
+mov1 = input("Enter first movie name:")
+mov2 = input("Enter second movie name:")
+mov3 = input("Enter third movie name:")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
